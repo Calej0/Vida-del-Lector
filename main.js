7c9a6b7f3221f47d33aca9d8eebee54b7663d475
Calej0 <112588137+Calej0@users.noreply.github.com>
2022-08-31 19:13:50 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+const contenido = document.getElementById("contenido");
+
+contenido.innerHTML = "hola";
+
+let nombre = "Carlos Bajaña";
+let edad =17;
+
+const COLOR_ROJO = "#FF0000";
+
+let resultado = "nombre:"+ nombre;
+
+
+
+nombre= "Bajaña Carlos";
+
+
